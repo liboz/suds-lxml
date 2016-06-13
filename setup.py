4e98298fb18354dc158b0e43d112d002e2758a37
liboz <ringil.sword@gmail.com>
2016-06-13 14:54:31 -0400

    bump version

--- setup.py
+++ setup.py
@@ -661,16 +661,17 @@ Lightweight SOAP client with lxml parser (Libo's fork).
 
   Based on the original 'suds' project by Jeff Ortel (jortel at redhat
 dot com) hosted at 'http://fedorahosted.org/suds' and 'suds-jurko' by 
-Jurko Gospodnetić ( jurko.gospodnetic@pke.hr ) hosted at https://bitbucket.org/jurko/suds.
+Jurko Gospodnetić ( jurko.gospodnetic@pke.hr ) 
+hosted at https://bitbucket.org/jurko/suds.
 
   'Suds' is a lightweight SOAP-based web service client for Python
 licensed under LGPL (see the LICENSE.txt file included in the
 distribution).
-
-  This is hopefully just a temporary fork of the original suds Python
-library project created because the original project development seems
-to have stalled. Should be reintegrated back into the original project
-if it ever gets revived again.
+  
+  One major problem with suds is that it is slow. 
+This is a fork of suds that tries to solve it by parse the xml returned 
+with the **fast** lxml parser instead of the default suds implementation.
+It requires lxml to be installed.
 
 """
 
